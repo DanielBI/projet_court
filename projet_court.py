f16d1011cc368c7596f876aa1f7e411e67f65d20
DanielBI <42939235+DanielBI@users.noreply.github.com>
2018-09-12 16:26:29 +0200

    Add files via upload
    Liste des coordonnées des atomes d'intérêt pour chaque résidu

--- projet_court.py
+++ projet_court.py
@@ -1,69 +1,36 @@
-representative_atoms = ["CA",  "N", "C", "O" , "H"]
+atomes_dinteret = ["CA",  "N", "C", "O" , "H"]
+
 
 
 def parse_pdb(pdbfile):
 	dico_residu={}
 	liste_residu=[]
 	liste_atome=[] 
+	num_residu=1
 	with open(pdbfile, 'r') as inputfile:
 		compteur=0
+		dico_atome={} 
 		for line in inputfile:
-			current_residu = line[22:26].strip()
-			dico_atome={} 
-			if line[0:6].strip() == "ATOM" and line[12:16].strip() in representative_atoms:
-				new_residu = line[22:26].strip()
-				if new_residu == current_residu:
+			if line[0:6].strip() == "ATOM" and line[12:16].strip() in atomes_dinteret:
 					dico_coord={}
 					dico_coord['num_residu'] = line[22:26].strip()
 					dico_coord['x'] = float(line[30:38])
 					dico_coord['y'] = float(line[38:46])
 					dico_coord['z'] = float(line[46:54])
-					dico_atome[line[12:16].strip()]=dico_coord
-					compteur=compteur + 1
-					liste_atome.append(dico_atome)
-				#liste_atome.append(dico_atome)
-				#compteur=compteur+1
-				#if compteur > 5:
-				#dico_residu[line[25:28].strip()]=liste_atome
-				#compteur=0
-				#liste_atome=[]
-	num_residu=1
-	liste1=[]
-	num_residu=1
-	print (liste_atome[1]['CA'])
-	for i in range(len(liste_atome)):
-		for atome in representative_atoms:
-			print(atome)
-			if liste_atome[i][atome][num_residu] == num_residu:
-				liste1.append(liste_atome[i][atome][num_residu])
-			num_residu = num_residu + 1 
-		liste_residu.append(liste1)
-		liste1=[]
-		num_residu=num_residu+1
-	return dico_atome
+					if line[12:16].strip() not in dico_atome and int(dico_coord['num_residu']) == num_residu:						
+						dico_atome[line[12:16].strip()]=dico_coord
+					else:						
+						num_residu=int(line[22:26].strip())
+						liste_atome.append(dico_atome)
+						dico_atome={}
+	print(liste_atome[0])
+	return liste_atome
 
 #dico_residu[line[22:26].strip()]=dico_atome[line[14:16].strip()]=dico_coord
 
 
-test=parse_pdb("/home/sdv/m2bi/dde_murat/projet_court/1bta.pdb")
+test=parse_pdb("1bta.pdb")
 
-
-#with open("/home/sdv/m2bi/dde_murat/projet_court/1bta.pdb", "r") as pdb_file:
- #   res_count = 0
-  #  for line in pdb_file:
-   #     if line.startswith("ATOM"):
-    #        atom_name = line[12:16].strip()
-     #       if atom_name in representative_atoms:
-      #          res_count += 1
-       #         res_name = line[17:20].strip()
-        #        res_num = int(line[22:26])
-         #       x = float(line[30:38])
-          #      y = float(line[38:46])
-           #     z = float(line[46:54])
-            #    el = (line[76:78])
-                #print(res_name, res_num, atom_name, x, y, z, el)
-             #   test=parse_pdb("/home/sdv/m2bi/dde_murat/projet_court/1bta.pdb")
-                #print(test)
 
 
 
