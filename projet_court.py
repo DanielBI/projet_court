090bd472c2a92b0c91945147f9a80e40441bfb3c
DanielBI <42939235+DanielBI@users.noreply.github.com>
2018-09-15 16:24:49 +0200

    Add files via upload

--- projet_court.py
+++ projet_court.py
@@ -1,16 +1,20 @@
-import math
+#dssp -i 1bta.pdb
 
+import math
 
 def parse_pdb(pdbfile):
 	dico_residu={}
 	liste_atome=[] 
-	num_residu=1
+	#num_residu=1
 	with open(pdbfile, 'r') as inputfile:
 		dico_atome={} 
 		for line in inputfile:
+			if line[9:12].strip()=='1' and line[0:6].strip() == "ATOM":
+				num_residu=int(line[22:26].strip())
 			if line[0:6].strip() == "ATOM" and line[12:16].strip() in ["N", "C", "O" , "H"]:
 					dico_coord={}
 					dico_coord['nom_residu'] = line[17:20].strip()
+					dico_coord['chaine'] =  line[21:23].strip()
 					dico_coord['num_residu'] = line[22:26].strip()
 					dico_coord['x'] = float(line[30:38])
 					dico_coord['y'] = float(line[38:46])
@@ -23,7 +27,9 @@ def parse_pdb(pdbfile):
 						liste_atome.append(dico_atome)
 						dico_atome={}
 						dico_atome[line[12:16].strip()]=dico_coord
-	liste_atome.append(dico_atome)							
+	
+	liste_atome.append(dico_atome)
+	print(liste_atome[16])
 	return liste_atome
 
 def calcul_distance(coord1, coord2):
@@ -43,30 +49,48 @@ def calcul_energie(residu1, residu2):
 		E = q1*q2*(1/rON + 1/rCH - 1/rOH - 1/rCN) * 332
 	return E
 
+def liaisons_consecutives(dico_liaisons):
+	liaisons_consecutives={}
+	for key in dico_liaisons:
+		if key + 1 in dico_liaisons or key - 1 in dico_liaisons:
+			liaisons_consecutives[key+1]=dico_liaisons[key]
+	return liaisons_consecutives
+			
+
 def calcul_helices(liste_atome):
-	helice_alpha={}
-	helice_310={}
-	helice_pi={}
+	liaisons_alpha={}
+	liaisons_310={}
+	liaisons_pi={}
+	helices_alpha={}
+	helices_310={}
+	helices_pi={}
+	helices_totales={}
 	for k in [3, 4, 5]: 
-		for i in range(len(liste_atome) - k):  
-			if liste_atome[i]['O']['nom_residu']=='PRO' or liste_atome[i+k]['C']['nom_residu']=='PRO':
+		for i in range(len(liste_atome) - k):
+			if liste_atome[i+k]['C']['nom_residu']=='PRO':
 				E = 0
 			else :  
 				E = calcul_energie(liste_atome[i], liste_atome[i+k])
-				if k == 3 : helice_310[i] = E
-				elif k == 4 : helice_alpha[i] = E 
-				elif k == 5 : helice_pi[i] = E
-#liste=[len(helice_alpha), len(helice_310), len(helice_pi)]
-	#MAX = max(liste)
-	#for i in range(MAX): 
-	#	print(i, helice_alpha[i], helice_310[i], helice_pi[i])
-	#	if i not in helice_alpha :
-					
-	return helice_310
+				if E < -0.5 :
+					if k == 3 : liaisons_310[i] = E
+					elif k == 4 : liaisons_alpha[i] = E 
+					elif k == 5 : liaisons_pi[i] = E
+	for key in liaisons_alpha:
+		print(key,liste_atome[key]['C']['nom_residu'])	
+	helices_alpha=liaisons_consecutives(liaisons_alpha)
+	helices_310=liaisons_consecutives(liaisons_310)
+	helices_pi=liaisons_consecutives(liaisons_pi)
+	helices_totales['helices_pi']=helices_pi
+	helices_totales['helices_310']=helices_310
+	helices_totales['helices_alpha']=helices_alpha
+	print(helices_totales['helices_alpha'])
+	return helices_alpha
 
-def calcul_feuillet_parallele(liste_atome):
+def calcul_feuillet(liste_atome):
 	Energie_feuillet_parallele={}
 	Energie_feuillet_antiparallele={}
+	feuillets_paralleles={}
+	feuillets_antiparalleles={}
 	for i in range(1, len(liste_atome)-1):
 		for j in range (i+2, len(liste_atome)-1):
 			#feuillets parallèles
@@ -92,15 +116,17 @@ def calcul_feuillet_parallele(liste_atome):
 			if A1 < -0.5 and A2 < -0.5 or A3 < -0.5 and A4 < -0.5:
 				Energie_feuillet_antiparallele[i]=k
 				continue
-	print(Energie_feuillet_parallele)
-	print(Energie_feuillet_antiparallele)
+	feuillets_paralleles=liaisons_consecutives(Energie_feuillet_parallele)
+	feuillets_antiparalleles=liaisons_consecutives(Energie_feuillet_antiparallele)
+	print(feuillets_paralleles)
+	print(feuillets_antiparalleles)
 	return Energie_feuillet_parallele
 
 
 liste=[]
-liste=parse_pdb("1bta.pdb")
+liste=parse_pdb("1mh1_withH.pdb")
 Energie_helice=calcul_helices(liste)
-Energie_feuillet = calcul_feuillet_parallele(liste)
+Energie_feuillet = calcul_feuillet(liste)
 
 
 
